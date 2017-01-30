0ca1176471a3d7ddb4fc415d4c20715da4ddc79d
MA047WA <MA047WA@PB-0986.pbi.global.pvt>
2017-01-30 17:11:07 +0100

    I added it to from a branch

--- gs-spring-boot/src/main/java/hello/Application.java
+++ gs-spring-boot/src/main/java/hello/Application.java
@@ -13,6 +13,7 @@ public class Application {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
+        System.out.println("I added it to from a branch");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
